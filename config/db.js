f8a71f6f4a88d89928e2a3394bb0413c4f8f0d90
tfoster-laptop <tfos262@wgu.edu>
2025-07-06 13:28:56 -0400

    temporarily bypass ssl - azure debugging

--- config/db.js
+++ config/db.js
@@ -1,23 +1,23 @@
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
-import fs from 'fs';
+// import fs from 'fs';
 
 dotenv.config();
 // // Azure MySQL connection example
 
 // var fs = require('fs');
 // var mysql = require('mysql');
-const serverCa = [fs.readFileSync("/var/www/html/DigiCertGlobalRootCA.crt.pem", "utf8")];
+// const serverCa = [fs.readFileSync("/var/www/html/DigiCertGlobalRootCA.crt.pem", "utf8")];
 var conn=mysql.createConnection({
     host:process.env.DB_HOST,
     user:process.env.DB_USER,
     password:process.env.DB_PASSWORD,
     database:process.env.DB_NAME,
     port:3306,
-    ssl: {
-        rejectUnauthorized: true,
-        ca: serverCa
-    }
+    // ssl: {
+    //     rejectUnauthorized: true,
+    //     ca: serverCa
+    // }
 });
 conn.connect(function(err) {
   if (err) throw err;
