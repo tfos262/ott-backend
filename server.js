3a53684473ac1693632286b2fa057d399ec65f76
tfoster-laptop <tfos262@wgu.edu>
2025-07-06 14:55:21 -0400

    add default route to app.listen()

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ async function startServer() {
 
         const PORT = process.env.PORT || 8080;
 
-        app.listen(PORT, () => {
+        app.listen(PORT, '0.0.0.0', () => {
             console.log(`Server is running on port ${PORT}`);
         });
     } catch (error) {
