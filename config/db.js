36520f29943e420c8495b3b214995f97a925835c
tfos262 <165966761+tfos262@users.noreply.github.com>
2026-02-01 14:45:41 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -8,19 +8,43 @@ dotenv.config();
 // var fs = require('fs');
 // var mysql = require('mysql');
 // const serverCa = [fs.readFileSync("/var/www/html/DigiCertGlobalRootCA.crt.pem", "utf8")];
-export async function createDbConnection() {
-  const conn = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: 3306,
-    // ssl: optional, uncomment for Azure
-    // ssl: {
-    //   rejectUnauthorized: true,
-    //   ca: [fs.readFileSync('/path/to/ca.pem', 'utf8')]
-    // }
-  });
+// export async function createDbConnection() {
+//   const conn = await mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     port: 3306,
+//     // ssl: optional, uncomment for Azure
+//     // ssl: {
+//     //   rejectUnauthorized: true,
+//     //   ca: [fs.readFileSync('/path/to/ca.pem', 'utf8')]
+//     // }
+//   });
+
+//   return conn;
+// }
+let conn;
+
+export function createDbConnection() {
+  if (!conn) {
+    conn = mysql.createPool({
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+
+      connectionLimit: 5,
+      waitForConnections: true,
+      queueLimit: 0,
+
+      ssl: {
+        rejectUnauthorized: true,
+        ca: process.env.DB_CA_CERT
+      }
+    });
+  }
 
   return conn;
 }
